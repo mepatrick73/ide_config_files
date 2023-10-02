863bac1a8f405644534e0c1b8f5b12e3dc4334be
mepatrick <pameu17@ulaval.ca>
2023-10-02 14:47:51 -0400

    Adding Proper Intellisense + VI mode + updating gitignore

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,3 @@
-Install-Module -Name PSReadLine -Force
-Install-Module -Name Terminal-Icons -Repository PSGallery
 function prompt {
   $loc = $executionContext.SessionState.Path.CurrentLocation;
 
@@ -11,3 +9,24 @@ function prompt {
   return $out
 }
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\clean-detailed.omp.json" | Invoke-Expression
+Import-Module -Name Terminal-Icons
+if ($host.Name -eq 'ConsoleHost')
+{
+    Import-Module PSReadLine
+    $OnViModeChange = [scriptblock]{
+    if ($args[0] -eq 'Command') {
+        # Set the cursor to a blinking block.
+        Write-Host -NoNewLine "`e[1 q"
+    }
+    else {
+        # Set the cursor to a blinking line.
+        Write-Host -NoNewLine "`e[5 q"
+    }
+    }
+    Set-PsReadLineOption -EditMode Vi 
+    Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $OnViModeChange
+    Set-PSReadlineKeyHandler -Chord Ctrl+n   -Function HistorySearchBackward 
+    Set-PSReadlineKeyHandler -Chord Ctrl+p -Function HistorySearchForward 
+    Set-PSReadlineKeyHandler -Chord Ctrl+" "   -Function Complete 
+    Set-PSReadLineKeyHandler -Chord Ctrl+u    -Function RevertLine
+}
