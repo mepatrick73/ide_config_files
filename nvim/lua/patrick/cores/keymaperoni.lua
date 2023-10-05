bc5cce4336b7d28496d37aa3e0cf54c938ee87cf
mepatrick <pameu17@ulaval.ca>
2023-10-05 16:03:22 -0400

    adding new plugins to neovim

--- nvim/lua/patrick/cores/keymaperoni.lua
+++ nvim/lua/patrick/cores/keymaperoni.lua
@@ -1,6 +1,9 @@
 local builtin = require('telescope.builtin')
+
+vim.keymap.set('n', '<leader>w', '<C-w>') 
+
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>pr', function()
+vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ")});
 end)
---
+++ nvim/lua/patrick/plugins/undotree_config.lua
@@ -0,0 +1,7 @@
+return{
+	"mbbill/undotree",
+	config=true,
+	keys = {
+		{ "<leader>u",":UndotreeToggle<CR>", desc = "Mark file with harpoon" }
+	}
+}
